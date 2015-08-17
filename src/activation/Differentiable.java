c10de78c8e3c1e4d736121fc1bdc7f1dd030198b
willsherwood <will.sherwood5@gmail.com>
2015-08-16 21:10:55 -0500

    Added an array iterating default method to the Differentiable interface

--- src/activation/Differentiable.java
+++ src/activation/Differentiable.java
@@ -1,18 +1,26 @@
 package activation;
 
-import java.util.function.Function;
+import java.util.function.DoubleFunction;
 
-public interface Differentiable<T> {
+public interface Differentiable {
 
     /**
      * @return the derivative of the function
      */
-    Function<T, T> derivative ();
+    DoubleFunction<Double> derivative ();
 
     /**
      * @return the numerical value of the derivative
      * of the function when applied with the input 'value'.
      * This is a less expensive call than derivative().
      */
-    T apply (T value);
+    Double apply (Double value);
+
+    default double[] apply (double[] values) {
+        double[] out = new double[values.length];
+        for (int i = 0; i < out.length; i++)
+            out[i] = apply(values[i]);
+        return out;
+    }
+
 }
